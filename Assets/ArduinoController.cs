74372c870e792e332aa28063d1c9760fd2bfdb82
YirenCC <yiren.cao@uoit.net>
2018-01-30 13:53:41 -0500

    with control script
    new script works/ problem not fixed/ in unity, freeze object rotation in x,y,z

--- Assets/ArduinoController.cs
+++ Assets/ArduinoController.cs
@@ -8,69 +8,57 @@ public class ArduinoController : MonoBehaviour
 {
     SerialPort serial = new SerialPort("COM4", 9600);
 
-    public int value = 0;
-
-    Rigidbody m_Rigidbody;
-    public float gravity;
-    public float speed;
-    bool m_Grounded;
-    public float m_MaxSpeed;
-    public float jumpSpeed;
+    public bool a, b, c, d = false;
 
     // Use this for initialization
     void Start()
     {
         serial.Open();
-        m_Rigidbody = gameObject.GetComponent<Rigidbody>();
-        m_Grounded = true;
 
         StartCoroutine
-(
+    (
     AsynchronousReadFromArduino
     ((string s) => inputCallback(s),     // Callback
         () => Debug.LogError("Error!"), // Error callback
         10000f                          // Timeout (milliseconds)
     )
-);
+    );
     }
 
 
     void inputCallback(string s)
     {
-        string buttonS = serial.ReadLine();
-        value = int.Parse(buttonS);
-        if ((value == 1) && m_Grounded)
+        if (s == "1")
         {
-            m_Rigidbody.velocity += transform.up * jumpSpeed * Time.deltaTime;
-            m_Grounded = false;
+            a = true;
         }
-        if ((value == 2) && m_Grounded == false)
+        if (s == "2")
         {
-            m_Rigidbody.velocity -= transform.up * jumpSpeed * Time.deltaTime;
-            m_Grounded = true;
+            b = true;
         }
-        if ((value == 3))
+        if (s == "3")
         {
-            m_Rigidbody.velocity += transform.forward * Time.deltaTime * speed;
-            float l_y = m_Rigidbody.velocity.y;
-            m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, m_MaxSpeed);
-            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, l_y, m_Rigidbody.velocity.z);
+            c = true;
         }
-        if ((value == 4))
+        if (s == "4")
         {
-            m_Rigidbody.velocity += -transform.forward * Time.deltaTime * speed;
-            float l_y = m_Rigidbody.velocity.y;
-            m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, m_MaxSpeed);
-            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, l_y, m_Rigidbody.velocity.z);
+            d = true;
+        }
+        if (s == "9")
+        {
+            a = false;
+            b = false;
+            c = false;
+            d = false;
         }
 
     }
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
 
         return;    
-    }
+    }*/
 
     public IEnumerator AsynchronousReadFromArduino(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity)
     {
@@ -118,7 +106,6 @@ public class ArduinoController : MonoBehaviour
             if (Vector3.Dot(transform.up, collision.contacts[0].normal) > 0.5f)
             {
                 Debug.Log("Grounded");
-                m_Grounded = true;
             }
         }
     }
---
+++ Assets/Control.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+    Rigidbody m_Rigidbody;
+    public float gravity;
+    public float speed;
+    public float m_MaxSpeed;
+    // Use this for initialization
+    void Start () {
+        m_Rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.GetComponent<ArduinoController>().a == true)
+        {
+            m_Rigidbody.velocity += transform.forward * Time.deltaTime * speed;
+            float l_y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, m_MaxSpeed);
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, l_y, m_Rigidbody.velocity.z);
+        }
+        if (gameObject.GetComponent<ArduinoController>().b == true)
+        {
+            m_Rigidbody.velocity += -transform.forward * Time.deltaTime * speed;
+            float l_y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, m_MaxSpeed);
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, l_y, m_Rigidbody.velocity.z);
+        }
+        if (gameObject.GetComponent<ArduinoController>().c == true)
+        {
+            m_Rigidbody.velocity += transform.forward * Time.deltaTime * speed;
+            float l_y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, m_MaxSpeed);
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, l_y, m_Rigidbody.velocity.z);
+        }
+        if (gameObject.GetComponent<ArduinoController>().d == true)
+        {
+            m_Rigidbody.velocity += -transform.forward * Time.deltaTime * speed;
+            float l_y = m_Rigidbody.velocity.y;
+            m_Rigidbody.velocity = Vector3.ClampMagnitude(m_Rigidbody.velocity, m_MaxSpeed);
+            m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, l_y, m_Rigidbody.velocity.z);
+        }
+    }
+}
